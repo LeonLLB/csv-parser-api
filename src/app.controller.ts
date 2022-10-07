d6c3fd9100077dd37621b52c313a353f691729f9
Leonard Velásquez <leonard.avp@gmail.com>
2022-10-07 11:46:46 -0400

    Generacion de csv en string

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, HttpStatus, ParseFilePipeBuilder, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  
+  //READS CSV
+  @Post('import')
+  @UseInterceptors(FileInterceptor('file'))  
+  importCSV(
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+      .addFileTypeValidator({
+        fileType: new RegExp('.csv$')
+      })  
+      .addMaxSizeValidator({
+        maxSize: 1024 * 1024 * 10 //10MB
+      })
+      .build({
+        errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY
+      }),    
+    ) file: Express.Multer.File,
+  ){
+    return this.appService.parseCSV(file)
+  }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  //GENERATES CSV
+  @Post('export')
+  exportCSV(@Body() input: {[x:string]:any}[]){
+    return this.appService.formatCSV(input)
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,45 @@
+import { Readable } from 'stream';
+import fs from 'fs'
 import { Injectable } from '@nestjs/common';
+import * as csv from 'fast-csv'
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  parseCSV(file: Express.Multer.File){
+
+    console.log(file)
+    
+  }
+
+  async formatCSV(inputs: {[x:string]:any}[]){
+
+    // const csvStream = csv.format()
+
+    // csvStream.pipe(process.stdout)
+
+    const rows: any[] = []
+    const headers: any[] = [] 
+
+    let i = 0
+    let j = 0
+    for (const input of inputs) {
+      j=i
+      for (const inputMap of Object.entries(input)) {
+        if(!headers.includes(inputMap[0])) headers.push(inputMap[0])
+        if(j===i){
+          rows.push([])
+        }
+        rows[i].push(inputMap[1])
+        j++
+      }
+      i++
+    }
+
+    const result = await csv.writeToString(rows,{headers})
+    console.log(result)
+
+    return rows
+
   }
 }
